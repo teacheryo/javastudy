71b2a397322c579a7702a3f99b86751e887eeda8
lhk <teacheryo@naver.com>
2020-10-16 17:44:31 +0900

    helloworld, variableex append

---
+++ HelloWorld/src/com/naver/MainEx.java
@@ -0,0 +1,32 @@
+package com.naver;
+
+public class MainEx {
+
+	public static void main(String[] args) {
+		System.out.println("good moring");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		System.out.println("good morning");
+		
+	}
+
+}
---
+++ VariableEX/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,41 @@
+package kr.co.ezen;
+
+public class MainEx {
+	
+	public static void main(String[] args) {
+		
+		// "morning"을  msg4  변수에 대입하시고
+		// 코드에 이를 적용하세요.
+		// 변수 선언 공식
+		// 자료형 변수명 = 자료형에 맞는 값;
+		
+		String msg = "true";
+		String msg2 = "11";
+		String msg3 = "good";
+		
+		
+		System.out.println(msg);
+		System.out.println(msg2);
+		System.out.println(msg3);
+		System.out.println("morning");
+		System.out.println(msg);
+		System.out.println(msg3);
+		System.out.println(msg2);
+		System.out.println(msg);
+		
+		
+		byte zzz = -128;
+		long aaa = 2500000000L;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
